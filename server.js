67bfe426bd2c7b3c67986803cec0eecc91ecbf99
hamumeron <nekosuke.1012@outlook.jp>
2025-07-29 07:47:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -30,3 +30,8 @@ app.get("/proxy", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Proxy server running on port ${PORT}`));
+
+app.get("/", (req, res) => {
+  res.send("I'm alive!");
+});
+
